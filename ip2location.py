e3b79061a7df034a813f016daceb5157d71ed65c
Blackwin <91410860+bl4ckwin@users.noreply.github.com>
2022-01-03 11:35:08 +0330

    Add files via upload

---
+++ ip2location.py
@@ -0,0 +1,67 @@
+from os import name
+from subprocess import run
+from colorama import Fore
+from requests import get
+from sys import exit , argv
+from time import sleep
+import json
+if name == 'nt':
+	run('cls',shell=True)
+else:
+	run('clear',shell=True)
+blue = Fore.BLUE
+white = Fore.WHITE
+if len(argv) == 1:
+    if name == 'nt':
+        run('cls',shell=True)
+    else:
+        run('clear',shell=True)
+    print(blue+'''
+ip2location script v 1.0
+coded by bl4ckwin
+
+usage:
+python -i <ip address>
+'''+white)
+    exit()
+else:
+    pass
+url = 'https://ipwhois.app/json/'
+if argv[1] == '-i':
+    pass
+else:
+    if name == 'nt':
+        run('cls',shell=True)
+    else:
+        run('clear',shell=True)
+    print(blue+'''
+ip2location script v 1.0
+coded by bl4ckwin
+
+usage:
+python -i <ip address>
+'''+white)
+    exit()
+ip_address = argv[2]
+data = get(url+str(ip_address))
+data = json.loads(data.text)
+msg = data['success']
+if msg == 'false':
+     print(blue+'[!] '+white+'Error , Please Check The IP Address Or Internet Connection !')
+else:
+    pass
+lat = data['latitude']
+lon = data['longitude']
+continent = data['continent']
+country = data['country']
+region = data['region']
+city = data['city']
+isp = data['isp']
+print('IP Address : '+str(ip_address))
+print('Internet Service Pervoider : '+str(isp))
+print('Continent : '+str(continent))
+print('Country : '+str(country))
+print('Region : '+str(region))
+print('City : '+str(city))
+print('Latitude : '+str(lat))
+print('Longitude : '+str(lon))
